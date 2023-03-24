e8eb4abb8916dfd266a5e275ab97d0487121bc49
carlo <carloszumbadocardenas@gmail.com>
2023-03-24 10:59:26 -0600

    Semana 10

--- src/main/java/com/tienda_v1/controller/IndexController.java
+++ src/main/java/com/tienda_v1/controller/IndexController.java
@@ -2,6 +2,8 @@ package com.tienda_v1.controller;
 
 
 
+import com.tienda_v1.service.ArticuloService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,10 +12,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class IndexController {
     
+    @Autowired
+    private ArticuloService articuloService;
     
     
     @GetMapping("/") //mapeo de una ruta//
     public String inicio(Model model){
+        var articulos=articuloService.getArticulos(false);
+        model.addAttribute("articulos",articulos);
+        
         return "index";
     }
 }
