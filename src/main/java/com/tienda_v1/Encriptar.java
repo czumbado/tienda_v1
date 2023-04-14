270eaa7f33ec32599aef050896e61c03497a5c58
carlo <carloszumbadocardenas@gmail.com>
2023-04-14 16:27:02 -0600

    Implementacion de carrito, reportes y seguridad

---
+++ src/main/java/com/tienda_v1/Encriptar.java
@@ -0,0 +1,17 @@
+package com.tienda_v1;
+
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class Encriptar {
+    public static void main(String[] args) {
+        var codigo = new BCryptPasswordEncoder();
+        System.out.println("Juan (123): " + codigo.encode("123"));
+        System.out.println("Rebeca (456): " + codigo.encode("456"));
+        System.out.println("Pedro (789): " + codigo.encode("789"));
+        System.out.println("Carlos (Prueba.): " + codigo.encode("Prueba."));
+
+    }
+
+}
+
